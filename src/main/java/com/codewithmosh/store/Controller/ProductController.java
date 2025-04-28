611f2067b2caeb831025030ea008c77e69861cd5
anonymousboy67 <aashishad67@gmail.com>
2025-04-28 12:19:30 +0545

    Building Product Endpoints

---
+++ src/main/java/com/codewithmosh/store/Controller/ProductController.java
@@ -0,0 +1,39 @@
+package com.codewithmosh.store.Controller;
+
+import com.codewithmosh.store.dtos.ProductDto;
+import com.codewithmosh.store.entities.Product;
+import com.codewithmosh.store.mappers.ProductMapper;
+import com.codewithmosh.store.repositories.ProductRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/products")
+public class ProductController {
+
+    private final ProductRepository productRepository;
+    private final ProductMapper productMapper;
+
+    @GetMapping
+    public List<ProductDto> getAllProducts(@RequestParam(name = "categoryId", required = false) Byte categoryId) {
+        List<Product> products;
+
+        if (categoryId != null) {
+            products = productRepository.findByCategoryId(categoryId);
+        } else {
+            products = productRepository.findAll();
+        }
+
+        return products.stream()
+                .map(productMapper::toDto)
+                .collect(Collectors.toList());
+    }
+
+}
