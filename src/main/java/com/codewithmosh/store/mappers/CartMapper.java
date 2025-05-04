8befc0076e3f2b1630ab6d67588f09de6b04389b
anonymousboy67 <aashishad67@gmail.com>
2025-05-04 09:18:28 +0545

    creating the cart component for Post

---
+++ src/main/java/com/codewithmosh/store/mappers/CartMapper.java
@@ -0,0 +1,11 @@
+package com.codewithmosh.store.mappers;
+
+import com.codewithmosh.store.dtos.CartDto;
+import com.codewithmosh.store.entities.Cart;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface CartMapper {
+    CartDto toDto(Cart cart);
+
+}
